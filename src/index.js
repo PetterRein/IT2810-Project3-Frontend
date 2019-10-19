0b3caa56ecf636bebd9dd26582f08a1017aa9e2a
Petter Grø Rein <pettegre@stud.ntnu.no>
2019-10-19 18:06:33 +0200

    create a apollo client and inject into app #18

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API_URL
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />, document.getElementById('root')
+  </ApolloProvider>,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
