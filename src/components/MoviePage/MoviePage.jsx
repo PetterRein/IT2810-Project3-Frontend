698942410585eae93b314d720a799b65b46e8021
Petter Grø Rein <pettegre@stud.ntnu.no>
2019-10-19 18:38:34 +0200

    add moviePage component #21

---
+++ src/components/MoviePage/MoviePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Movie from '../Movie/Movie'
+import './MoviePage.css'
+
+export default function MoviePage ({movies}) {
+  const Movies = movies.map((movie, i) =>
+    <Movie movieInfo={movie} key={i}/>
+  );
+
+  return (
+    <div className={ 'rows' }>
+      { Movies }
+    </div>
+  )
+}
