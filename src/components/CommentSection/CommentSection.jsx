70670eccdd04ab1742fe40fca508fa61d90217d9
camelgod <god@e-grotto.faith>
2019-10-21 19:36:20 +0200

    Start working on commentsection #17
    g

---
+++ src/components/CommentSection/CommentSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+//import { useParams } from 'react-router-dom';
+import './CommentSection.css'; 
+//import { gql } from "apollo-boost";
+//import { useQuery } from "@apollo/react-hooks";
+
+export default function CommentSection (){
+
+  //let params = useParams();
+
+  return (
+	  <div className={'c6'}>
+		  <p>Todo comment section</p>
+	  </div>
+  )
+}
