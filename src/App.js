f3da0027be67916afbd653366ddc16119fe749e4
camelgod <god@e-grotto.faith>
2019-10-21 19:34:48 +0200

    Forgot app in previous commit. Changes done are related to styling and routes.  #12 #22
    f

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
-import MoviePage from './components/MoviePage'
+import MovieList from './components/MovieList'
+import MovieDetail from './components/MovieDetail'
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const movieQuery = gql`
   {
@@ -28,10 +31,16 @@ function App() {
   const movies = dataFilms.movies
   return (
     <div className="App">
-	<div class="header_bar">
-		<h3 class="head_text">Project 3</h3>
-	</div>
-	<MoviePage movies={movies}/>
+	  <Router>
+		  <div className="header_bar">
+			  <Link to="/movieList" className="link">Home</Link>
+		  </div>
+		  <Route path="/movieList" >
+			  <MovieList movies={movies}/>
+		  </Route>
+		  <Route path="/detail/:id" component={MovieDetail} >
+		  </Route>
+	  </Router>
     </div>
   );
 }
