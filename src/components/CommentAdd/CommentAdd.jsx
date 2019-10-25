c6f37dc1f243337cdbcb9e4934f9e9a74bef25e6
camelgod <god@e-grotto.faith>
2019-10-25 20:00:59 +0200

    Fix dynamic loading of comments

--- src/components/CommentAdd/CommentAdd.jsx
+++ src/components/CommentAdd/CommentAdd.jsx
@@ -4,56 +4,32 @@ import { useQuery, useMutation } from "react-apollo";
 import Comment from '../Comment/Comment';
 
 const ADD_COMMENT = gql`
-  mutation AddComment($movieid: ID!, $comment: String!) {
-    addMovieComment(movieid: $movieid, comment: $comment){
-      id
-      comment
-    }
-  }
-`;
+	  mutation AddComment($movieid: ID!, $comment: String!) {
+	    addMovieComment(movieid: $movieid, comment: $comment){
+	      id
+	      comment
+	    }
+	  }
+	`;
 
 export default function CommentAdd (props) {
-
-	const GET_COMMENTS = gql`
-	  {
-	    commentsForMovie(movieid: "${props.movieid}") {
-comment
-id
-    }
-  }
-`;
-
 	let input;
 
-	const { loading, error, data } = useQuery(GET_COMMENTS)
-	const [addComment, { commentdata }] = useMutation(ADD_COMMENT);
-
-	if (loading) return 'Loading...';
-	if (error) return `Error! ${error.message}`;
-	function reloadpage(e) {
-		window.location.reload();
-	}
+	const [addComment] = useMutation(ADD_COMMENT)
 
 	function handleSubmission(e) {
 		e.preventDefault();
 		addComment({ variables: { movieid: props.movieid, comment: input.value } });
 
 		input.value = '';
-		// Optimalt bruker vi Apollo her, men vanskelig med nåværende struktur
+		// Appollo sin Poll-funskjon vil automatisk hente inn nye comments etter submission
 	}
 	return (
 		<div>
 			<form onSubmit={ handleSubmission } >
 				<input ref={node => { input = node; }}/>
 				<button type="submit">Add Comment</button>
-				<a href="#" onClick={reloadpage}>Reload comments</a>
 			</form>
-			<p>Comments:</p>
-			<div>
-				{data.commentsForMovie.map(comment => (
-					<Comment key={comment.id} comment={comment.comment} />
-				))}
-			</div>
 		</div>
 	);
 }
--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -5,18 +5,49 @@ import gql from 'graphql-tag'
 import { Query } from 'react-apollo';
 import Comment from '../Comment/Comment';
 import CommentAdd from '../CommentAdd/CommentAdd';
+import { useQuery, useMutation } from "react-apollo";
 
-export default function CommentSection (){
-  let params = useParams();
 
-  return (
-    <div className={'commentBox page_box'}>
-	    <div class="box_header">
-		    <h3>Add a comment</h3>
-	    </div>
-	    <div class="box_content left-align">
-		    <CommentAdd movieid={params.id} />
-	    </div>
-    </div>
-  )
+export default function CommentSection (props){
+	const GET_COMMENTS = gql`
+	  {
+	    commentsForMovie(movieid: "${props.movie}") {
+		comment
+		id
+	    }
+	  }
+	`;
+
+
+	const { loading, error, data } = useQuery(GET_COMMENTS,
+		{
+			pollInterval: 1000,
+			// evt bruke Refetch i onClick
+		});
+
+	if (loading) return 'Loading...';
+	if (error) return `Error! ${error.message}`;
+
+	console.log(data.commentsForMovie);
+		let input;
+
+	return (
+		<div className={'commentBox page_box'}>
+			<div class="box_header">
+				<h3>Add a comment</h3>
+			</div>
+			<div class="box_content left-align">
+				<CommentAdd movieid={props.movie} />
+				<p>Comments:</p>
+				<div>
+					{data.commentsForMovie.map(({ id, comment }) => {
+						let input;
+						return (
+							<Comment key={id} comment={comment} />
+						)
+					})}
+				</div>
+			</div>
+		</div>
+	)
 }
